2f71d56b0db8b33c4e293a93758abbcc57cde649
Олександр Карпюк <sashakarpjuk43@gmail.com>
2024-09-28 12:54:49 +0300

    Created Database using Ef Core

---
+++ UrlShortenerInforceTestTask/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortenerInforceTestTask.Models;
+
+namespace UrlShortenerInforceTestTask.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<AppUser> AppUser { get; set; }
+        public DbSet<Urls> Urls { get; set; }
+
+    }
+}
