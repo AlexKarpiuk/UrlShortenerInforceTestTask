f63d454f86ce81e1fddf5cab5dbd30bf0ad1d436
Олександр Карпюк <sashakarpjuk43@gmail.com>
2024-09-28 12:44:04 +0300

    Add project files.

---
+++ UrlShortenerInforceTestTask/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿namespace UrlShortenerInforceTestTask.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAdmin { get; set; } = false;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+
+        public ICollection<Urls> Urls { get; set; }
+    }
+}
