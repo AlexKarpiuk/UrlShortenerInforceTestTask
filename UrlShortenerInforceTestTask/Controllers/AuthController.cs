6569fa37a9633d344737f56ba0cb8e37f92bb4f7
Олександр Карпюк <sashakarpjuk43@gmail.com>
2024-09-29 12:01:12 +0300

    Added Login

--- UrlShortenerInforceTestTask/Controllers/AuthController.cs
+++ UrlShortenerInforceTestTask/Controllers/AuthController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using UrlShortenerInforceTestTask.Data;
 using UrlShortenerInforceTestTask.Interfaces;
 using UrlShortenerInforceTestTask.Models;
@@ -11,10 +15,12 @@ namespace UrlShortenerInforceTestTask.Controllers
     public class AuthController : Controller
     {
         private readonly IAppUserRepository _appUserRepository;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAppUserRepository appUserRepository)
+        public AuthController(IAppUserRepository appUserRepository, IConfiguration config)
         {
             _appUserRepository = appUserRepository;
+            _config = config;
         }
 
         [HttpGet("Register")]
@@ -59,6 +65,62 @@ namespace UrlShortenerInforceTestTask.Controllers
             }
 
             return RedirectToAction("ShowURLsTable", "Urls");
+        }
+
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+
+            var email = loginViewModel.EmailAddress;
+            var password = loginViewModel.Password;
+
+            var user = await _appUserRepository.FindUserByEmailAsync(email);
+
+            if (user != null)
+            {
+                bool passwordCheck = PasswordHasher.HashPassword(loginViewModel.Password) == user.Password;
+                if (passwordCheck)
+                {
+                    bool userRole = user.IsAdmin;
+                    var claims = new List<Claim>() { new Claim("email", email) };
+                    if (userRole)
+                        claims.Add(new Claim("role", "admin"));
+                    else
+                        claims.Add(new Claim("role", "user"));
+
+                    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("Jwt:Key").Value));
+
+                    var signingCred = new SigningCredentials(securityKey,
+                        SecurityAlgorithms.HmacSha256);
+
+                    var securityToken = new JwtSecurityToken(
+                        claims: claims,
+                        expires: DateTime.Now.AddMinutes(20),
+                        issuer: _config.GetSection("Jwt:Issuer").Value,
+                        audience: _config.GetSection("Jwt:Audience").Value,
+                        signingCredentials: signingCred);
+
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(securityToken);
+                    await Console.Out.WriteLineAsync($"JWT: {tokenString}");
+                    HttpContext.Session.SetString("JWToken", tokenString);
+
+                    return RedirectToAction("ShortURLsTable", "Urls");
+                    
+                }
+                TempData["Error"] = "Wrong credentials. Please, check your password";
+                return View(loginViewModel);
+            }
+
+            TempData["Error"] = "Wrong credentials. Please, check your email or password";
+            return View(loginViewModel);
         }
 
     }
--- UrlShortenerInforceTestTask/Program.cs
+++ UrlShortenerInforceTestTask/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using UrlShortenerInforceTestTask.Data;
 using UrlShortenerInforceTestTask.Interfaces;
 using UrlShortenerInforceTestTask.Repositories;
@@ -13,6 +17,30 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+{
+    option.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        RequireExpirationTime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value)),
+        ValidIssuer = builder.Configuration.GetSection("Jwt:Issuer").Value,
+        ValidAudience = builder.Configuration.GetSection("Jwt:Audience").Value,
+    };
+});
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+       .AddCookie();
 
 
 var app = builder.Build();
@@ -30,6 +58,18 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+app.Use(async (context, next) =>
+{
+    var JWToken = context.Session.GetString("JWToken");
+    if (!string.IsNullOrEmpty(JWToken))
+    {
+        context.Request.Headers.Append("Authorization", "Bearer " + JWToken);
+    }
+    await next();
+});
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
